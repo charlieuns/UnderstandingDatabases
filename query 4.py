0fda8e0d3912297e70a76e009d730ba7ba869ded
w1ldfire1 <146308183+w1ldfire1@users.noreply.github.com>
2025-01-07 14:20:52 +0000

    Update query 4.py

--- query 4.py
+++ query 4.py
@@ -11,9 +11,11 @@ products_collection = db['Products']
 
 # Query 1: Mobile_phone Sales performance by brand
 sales_pipeline = [
+    # 1. Extract each product in the products array.
     {
         "$unwind": "$products"
     },
+    # 2. Match the product IDs in PastOrders with details.
     {
         "$lookup": {
             "from": "Products",
@@ -26,10 +28,12 @@ sales_pipeline = [
         "$unwind": "$product_details"
     },
     {
+    # 3. Only select the mobile phone product
         "$match": {
             "product_details.product_category": "mobile_phone"  # Filter by category
         }
     },
+    # 4. Group data by mobile phone brand, calculate the total sales
     {
         "$group": {
             "_id": "$product_details.other_product_details.mobile_phone.brand",
@@ -39,13 +43,15 @@ sales_pipeline = [
         }
     },
     {
+    # 5. Sorting by total sales in descending order
         "$sort": {"total_sales": -1}
     }
 ]
-
+# 6. Save query as Dataframe
 sales_data = list(past_orders_collection.aggregate(sales_pipeline))
 sales_df = pd.DataFrame(sales_data)
 
+# 7. Visualization: Use Seaborn to create bar chat
 sns.barplot(data=sales_df, x='_id', y='total_sales', palette="viridis",hue='_id',legend=False)
 for index, row in sales_df.iterrows():
     plt.text(index, row['total_sales'], f"${row['total_sales']:.2f}", ha='center', va='bottom', fontsize=10)
@@ -59,6 +65,7 @@ plt.show()
 
 # Query 2: Current Inventory performance
 inventory_pipeline = [
+    # 1. ​Matching fresh product IDs in the Inventory collection
     {
         "$lookup": {
             "from": "Products",
@@ -71,25 +78,34 @@ inventory_pipeline = [
         "$unwind": "$product_details"
     },
     {
+    # 2. Only select the mobile phone product
+        "$match": {
+            "product_details.product_category": "fresh_product"  # Filter by category
+        }
+    },
+    # 3. Group data by category, compute the total inventory for each category.
+    {
         "$group": {
-            "_id": "$product_details.product_category",
+            "_id": "$product_details.fresh_product_details.category",
             "total_inventory": {"$sum": "$inventory"}
         }
     },
+    # 4. Sorting by total sales in descending order.
     {
         "$sort": {"total_inventory": -1}
     }
 ]
-
+# 5. Save query as Dataframe
 inventory_data = list(inventory_collection.aggregate(inventory_pipeline))
 inventory_df = pd.DataFrame(inventory_data)
 
+# 6. Visualization: Use Seaborn to create bar chat
 sns.barplot(data=inventory_df, x='_id', y='total_inventory',palette="magma",hue='_id',legend=False)
 for index, row in inventory_df.iterrows():
     plt.text(index, row['total_inventory'], f"{row['total_inventory']}", ha='center', va='bottom', fontsize=10)
 
-plt.title('Current Inventory Levels by Product Category', fontsize=14)
-plt.xlabel('Product Category', fontsize=12)
+plt.title('Current Fresh Product Inventory Levels', fontsize=14)
+plt.xlabel('Fresh Product Category', fontsize=12)
 plt.ylabel('Total Inventory', fontsize=12)
 plt.xticks(rotation=45)
 plt.tight_layout()
