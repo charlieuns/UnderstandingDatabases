1993ab5d87026b4bb37003ea6c7b4be19d34478a
charlieuns <charlieunsworth55@gmail.com>
2025-01-07 17:08:18 +0000

    Update updating_avg_ratings.py

--- updating_avg_ratings.py
+++ updating_avg_ratings.py
@@ -13,19 +13,10 @@ def calculate_avg_rating(product_id):
     ratings = [rating['rating'] for rating in ratings_result]
     return sum(ratings) / len(ratings)
 
-# Computing ratings for fresh products
-fresh_products = list(products_collection.find({'product_category':'fresh_product'}, {'product_ID':1, '_id':0}))
+# Computing ratings for all products
+products = list(products_collection.find({}, {'product_ID':1, '_id':0}))
 
-for product in fresh_products:
+for product in products:
     product_ID = product['product_ID']
     avg_rating = calculate_avg_rating(product_ID)
-    products_collection.update_one({'product_ID':product_ID}, {"$set":{"fresh_product_details.avg_rating": avg_rating}})
-
-# Computing ratings for other products
-other_categories=['book','cd','mobile_phone','home_appliance']
-other_products = list(products_collection.find({'product_category':{"$in":other_categories}}, {'product_ID':1, '_id':0}))
-
-for product in other_products:
-    product_ID = product['product_ID']
-    avg_rating = calculate_avg_rating(product_ID)
-    products_collection.update_one({'product_ID':product_ID}, {"$set":{"other_product_details.avg_rating": avg_rating}})
+    products_collection.update_one({'product_ID':product_ID}, {"$set":{"average_rating": avg_rating}})
